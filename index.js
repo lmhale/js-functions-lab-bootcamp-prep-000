a893cc3487177e36cce31a877689478e36376c63
Lauren M Hale <lmhale710@gmail.com>
2018-01-22 03:12:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function happyHolidays(){
 function happyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
 }
-function happyHolidayTo(holday,name){
+function happyHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`;
 }
