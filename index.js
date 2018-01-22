32d4e4d3d7da78f9214252670d4fc0a903feeb5c
Lauren M Hale <lmhale710@gmail.com>
2018-01-22 03:14:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function happyHolidayTo(holiday,name){
   return `Happy ${holiday}, ${name}!`;
 }
 
-function fholidayCountdown(holiday, days) {
+function holidayCountdown(holiday, days) {
   return `It's ${days} until ${holiday}!`;
 
 }
